072cf998c940ea270cbfd128164cbef78bfdd329
ryantsai <rtsai0104@gmail.com>
2019-01-07 12:23:16 -0600

    Finish Wk5

---
+++ PA_Test/src/Week5/LCS2.java
@@ -0,0 +1,55 @@
+package Week5;
+
+import java.util.*;
+
+public class LCS2 {
+
+    private static int lcs2(int[] a, int[] b) {
+        //Write your code here
+        int[][] grid = new int[a.length+1][b.length+1];
+        for (int i = 0; i <= a.length; i++) {
+            grid[i][0] = 0;
+        }
+        for (int j = 0; j <= b.length; j++) {
+            grid[0][j] = 0;
+        }
+        int max = 0;
+        for (int i = 1; i <= a.length; i++) {
+            for (int j = 1; j <= b.length; j++) {
+                if(a[i-1] == b[j-1]) {
+                    grid[i][j] = grid[i-1][j-1]+1;
+                } else {
+                    grid[i][j] = Math.max(grid[i-1][j], grid[i][j-1]);
+                }
+                if(max < grid[i][j]) {
+                    max = grid[i][j];
+                }
+            }
+        }
+//        for (int i = 0; i <= a.length; i++) {
+////            for (int j = 0; j <= b.length; j++) {
+////                System.out.print(grid[i][j] + "|");
+////            }
+////            System.out.print("\n");
+////        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        int m = scanner.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = scanner.nextInt();
+        }
+
+        System.out.println(lcs2(a, b));
+    }
+}
+
---
+++ PA_Test/src/Week5/LCS3.java
@@ -0,0 +1,58 @@
+package Week5;
+
+import java.util.*;
+// Question 6 Wk 5
+public class LCS3 {
+
+    private static int lcs3(int[] a, int[] b, int[] c) {
+        //Write your code here
+        int[][][] grid = new int[a.length+1][b.length+1][c.length+1];
+        for (int i = 0; i <= a.length; i++) {
+            grid[i][0][0] = 0;
+        }
+        for (int j = 0; j <= b.length; j++) {
+            grid[0][j][0] = 0;
+        }
+        for (int k = 0; k <= b.length; k++) {
+            grid[0][k][0] = 0;
+        }
+        int max = 0;
+        for (int i = 1; i <= a.length; i++) {
+            for (int j = 1; j <= b.length; j++) {
+                for (int k = 1; k <= c.length; k++) {
+                    if(a[i-1] == b[j-1] && a[i-1] == c[k-1]) {
+                        grid[i][j][k] = grid[i-1][j-1][k-1]+1;
+                    } else {
+                        grid[i][j][k] = Math.max(grid[i-1][j][k],
+                                Math.max(grid[i][j-1][k], grid[i][j][k-1]));
+                    }
+                    if(max < grid[i][j][k]) {
+                        max = grid[i][j][k];
+                    }
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int an = scanner.nextInt();
+        int[] a = new int[an];
+        for (int i = 0; i < an; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int bn = scanner.nextInt();
+        int[] b = new int[bn];
+        for (int i = 0; i < bn; i++) {
+            b[i] = scanner.nextInt();
+        }
+        int cn = scanner.nextInt();
+        int[] c = new int[cn];
+        for (int i = 0; i < cn; i++) {
+            c[i] = scanner.nextInt();
+        }
+        System.out.println(lcs3(a, b, c));
+    }
+}
+
